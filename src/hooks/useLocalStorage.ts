8bba5d438970ccbd78c0a7c5eb7469eaeca3da22
Feliciano Ferreira <felizartpt@gmail.com>
2025-10-10 09:35:03 +0000

    Error: Maximum update depth exceeded. This can happen when a component r

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -6,6 +6,7 @@ import { useState, useEffect, Dispatch, SetStateAction, useCallback } from 'reac
 type SetValue<T> = Dispatch<SetStateAction<T>>;
 
 function useLocalStorage<T>(key: string, initialValue: T): [T, SetValue<T>] {
+  
   const readValue = useCallback((): T => {
     if (typeof window === 'undefined') {
       return initialValue;
@@ -19,11 +20,9 @@ function useLocalStorage<T>(key: string, initialValue: T): [T, SetValue<T>] {
     }
   }, [initialValue, key]);
 
-  // Use lazy state initialization to read from localStorage only once.
   const [storedValue, setStoredValue] = useState<T>(readValue);
 
-  const setValue: SetValue<T> = useCallback(
-    (value) => {
+  const setValue: SetValue<T> = value => {
       if (typeof window === 'undefined') {
         console.warn(
           `Tried setting localStorage key “${key}” even though environment is not a client`
@@ -31,34 +30,17 @@ function useLocalStorage<T>(key: string, initialValue: T): [T, SetValue<T>] {
         return;
       }
       try {
-        // Allow value to be a function so we have same API as useState
         const newValue = value instanceof Function ? value(storedValue) : value;
         window.localStorage.setItem(key, JSON.stringify(newValue));
         setStoredValue(newValue);
       } catch (error) {
         console.warn(`Error setting localStorage key “${key}”:`, error);
       }
-    },
-    [key, storedValue]
-  );
+    };
   
-  // This useEffect is to synchronize state between tabs.
   useEffect(() => {
-    const handleStorageChange = (event: StorageEvent) => {
-      if (event.key === key && event.newValue !== null) {
-        try {
-          setStoredValue(JSON.parse(event.newValue));
-        } catch(error) {
-            console.warn(`Error parsing storage change for key “${key}”:`, error)
-        }
-      }
-    };
-
-    window.addEventListener('storage', handleStorageChange);
-    return () => {
-      window.removeEventListener('storage', handleStorageChange);
-    };
-  }, [key]);
+    setStoredValue(readValue());
+  }, [readValue]);
 
 
   return [storedValue, setValue];
