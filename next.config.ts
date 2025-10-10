c37cb4f429e562aca3ef02ea2e1c9f7ffe344694
Feliciano Ferreira <felizartpt@gmail.com>
2025-10-10 09:01:45 +0000

    Error: Invalid src prop (https://placehold.co/600x400.png) on `next/imag

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
   typescript: {
     ignoreBuildErrors: false,
   },
