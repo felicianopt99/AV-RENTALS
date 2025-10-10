2fa6644392875957e2fc1442870217fc5645561f
Feliciano Ferreira <felizartpt@gmail.com>
2025-10-10 08:55:11 +0000

    its not starting the app ...averytm its like this find a definiive solut

--- next.config.ts
+++ next.config.ts
@@ -9,19 +9,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: false,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  experimental: {
-    allowedDevOrigins: ["https://6000-firebase-studio-1748510178971.cluster-oayqgyglpfgseqclbygurw4xd4.cloudworkstations.dev"],
-  },
 };
 
 export default nextConfig;
